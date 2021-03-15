b3d723c872deb701b0a1ce247dd372343fa9058e
Elio Struyf <elio@struyfconsulting.be>
2021-03-15 11:45:22 +0100

    Updated name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [15/03/2021] 0.0.2
+
+- Updated internal switch name
+
 ## [15/03/2021] 0.0.1
 
 - Initial test version
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { PresenceAccessory } from './accessories';
 import { Homebridge } from './models';
 const packageJSON = require('../package.json');
 
-const HOMEBRIDGE_PLATFORM_NAME = "presence-switch";
+const HOMEBRIDGE_PLATFORM_NAME = "presence-switch-slack";
 
 export default function (homebridge: Homebridge) {
   PresenceAccessory.register(homebridge, packageJSON, HOMEBRIDGE_PLATFORM_NAME);
